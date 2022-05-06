310769d4448e3eafd7a6fb07e3e2c27d3b0e82dc
Alex Kopareiko <sanyakopar@gmail.com>
2022-05-06 13:21:27 +0300

    Lesson 4.4 - For-Loops For Waves https://learn.unity.com/tutorial/lesson-4-4-for-loops-for-waves?uv=2020.3&pathwayId=5f7e17e1edbc2a5ec21a20af&missionId=5f7648a4edbc2a5578eb67df&projectId=5cf96846edbc2a2bcde6d0fc

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    private AudioSource playerAudio;
     private float powerUpStrength = 15.0f;
+
     public float speed = 5.0f;
     public bool hasPowerup = false;
     public GameObject powerupIndicator;
+    public AudioClip hitSound;
+    public AudioClip powerupSound;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         focalPoint = GameObject.Find("Focal Point");
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -36,6 +41,7 @@ public class PlayerController : MonoBehaviour
             powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
             StartCoroutine(PowerupCountdownRoutine());
+            playerAudio.PlayOneShot(powerupSound, 1.0f);
         }
     }
 
@@ -48,13 +54,17 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        if (collision.gameObject.CompareTag("Enemy") )
         {
-            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+            playerAudio.PlayOneShot(hitSound, 1.0f);
+            if(hasPowerup)
+            {
+                Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+                Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
 
-            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
-            Debug.Log("Collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+                enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+                Debug.Log("Collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+            }
         }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,25 +5,54 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9.0f;
+    public int enemyCount;
+    public int powerupCount;
+    public int waveNumber = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(false), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        powerupCount = GameObject.FindGameObjectsWithTag("Powerup").Length;
+
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(++waveNumber);
+            if(powerupCount == 0)
+            {
+                Instantiate(powerupPrefab, GenerateSpawnPosition(false), powerupPrefab.transform.rotation);
+            }
+        }
     }
 
-    private Vector3 GenerateSpawnPosition()
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    private Vector3 GenerateSpawnPosition(bool isEnemy = true)
     {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosY = 0;
+        if (isEnemy)
+        {
+            spawnPosY = Random.Range(0, spawnRange);
+        }
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
 
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        Vector3 randomPos = new Vector3(spawnPosX, spawnPosY, spawnPosZ);
 
         return randomPos;
     } 
